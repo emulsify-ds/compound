71011f6167fec1960e4397bb0f7e35a84486d98d
feynman <callin@mullaney.io>
2021-08-29 22:15:15 -0500

    feat: alignment/toggle/layout styles and functionality for base menu

---
+++ components/02-molecules/menu/menu.js
@@ -0,0 +1,45 @@
+Drupal.behaviors.menuToggle = {
+  attach(context) {
+    const menu = context.querySelector('.menu-with-toggle');
+
+    function toggleAria(el) {
+      let x = el.getAttribute('aria-expanded');
+      if (x == 'true') {
+        x = 'false';
+      } else {
+        x = 'true';
+      }
+      el.setAttribute('aria-expanded', x);
+    }
+
+    function toggleMenu(el, menuName) {
+      // Toggle aria or class for menu lists.
+      if (el.parentElement.hasAttribute('aria-expanded')) {
+        toggleAria(el.parentElement);
+      } else {
+        // Toggle open menu if expand/back button is clicked.
+        if (
+          el.nextElementSibling &&
+          el.nextElementSibling.classList.contains(
+            menuName + '__menu-list--sub',
+          )
+        ) {
+          el.nextElementSibling.classList.toggle('sub--open');
+        } else {
+          el.parentElement.parentElement.classList.toggle('sub--open');
+        }
+      }
+    }
+
+    // Toggle mobile menu classes when clicked.
+    if (menu) {
+      const toggleExpand = menu.getElementsByClassName(menu.id + '__toggle');
+
+      toggleExpand.forEach((item) => {
+        item.addEventListener('click', () => {
+          toggleMenu(item, menu.id);
+        });
+      });
+    }
+  },
+};
--- components/02-molecules/menu/menu.stories.js
+++ components/02-molecules/menu/menu.stories.js
@@ -2,9 +2,41 @@ import menuTwig from './menu.twig';
 
 import menuData from './menu.yml';
 
+import './menu';
+
 /**
  * Storybook Definition.
  */
-export default { title: 'Molecules/Menu' };
+export default {
+  title: 'Molecules/Menu',
+  argTypes: {
+    menuLayout: {
+      control: {
+        type: 'select',
+        options: ['inline', 'stacked'],
+      },
+      defaultValue: 'inline',
+    },
+    menuAlignment: {
+      control: {
+        type: 'select',
+        options: ['left', 'center', 'right'],
+      },
+      defaultValue: 'left',
+    },
+    menuToggle: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: false,
+    },
+  },
+};
 
-export const menu = () => menuTwig(menuData);
+export const menu = ({ menuLayout, menuAlignment, menuToggle }) =>
+  menuTwig({
+    ...menuData,
+    menu__layout: menuLayout,
+    menu__alignment: menuAlignment,
+    menu__toggle: menuToggle,
+  });
