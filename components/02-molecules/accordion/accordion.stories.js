6153cc7c0a4a9e2af1fe4d6f12afbae1e37251d3
Brian Lewis <brian@fourkitchens.com>
2021-10-01 12:28:43 -0500

    feat: added support for potition and alignment to accordion

--- components/02-molecules/accordion/accordion.stories.js
+++ components/02-molecules/accordion/accordion.stories.js
@@ -5,6 +5,41 @@ import accordionData from './accordion.yml';
 /**
  * Storybook Definition.
  */
-export default { title: 'Molecules/Accordion' };
+export default {
+  title: 'Molecules/Accordion',
+  argTypes: {
+    accordionLayout: {
+      control: {
+        type: 'select',
+        options: ['stacked', 'tooltip'],
+      },
+      defaultValue: 'stacked',
+    },
+    accordionPosition: {
+      control: {
+        type: 'select',
+        options: ['top', 'bottom'],
+      },
+      defaultValue: 'top',
+    },
+    accordionAlignment: {
+      control: {
+        type: 'select',
+        options: ['left', 'center', 'right'],
+      },
+      defaultValue: 'left',
+    },
+  },
+};
 
-export const accordion = () => accordionTwig(accordionData);
+export const accordion = ({
+  accordionLayout,
+  accordionPosition,
+  accordionAlignment,
+}) =>
+  accordionTwig({
+    ...accordionData,
+    accordion__layout: accordionLayout,
+    accordion__position: accordionPosition,
+    accordion__alignment: accordionAlignment,
+  });
