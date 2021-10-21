77903d6b46eb6e96407241f31e8790b25a3d915f
Brian Lewis <brian@fourkitchens.com>
2021-10-21 12:05:49 -0500

    chore: updated button story to support 'named' mappings

--- components/01-atoms/buttons/buttons.stories.js
+++ components/01-atoms/buttons/buttons.stories.js
@@ -22,9 +22,13 @@ export default {
       defaultValue: 'Button Text',
     },
     borderRadius: {
-      control: {
-        type: 'select',
-        options: ['None', '4px', '8px', '100vmax'],
+      type: 'select',
+      options: ['None', 'Some', 'More', 'A lot'],
+      mapping: {
+        None: '0px',
+        Some: '0.5rem',
+        More: '1rem',
+        'A lot': '100vmax',
       },
       defaultValue: 'None',
     },
