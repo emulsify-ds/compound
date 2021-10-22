54046b9ab47b472be0698228e93092306f717e18
Brian Lewis <brian@fourkitchens.com>
2021-10-22 05:37:34 -0500

    feat: support cozy/compact in storybook

--- components/00-base/00-defaults/settings.stories.js
+++ components/00-base/00-defaults/settings.stories.js
@@ -1,3 +1,6 @@
+import gridTwig from '../../03-organisms/grid/grid.twig';
+import gridData from '../../03-organisms/grid/grid.yml';
+
 /**
  * Storybook Definition.
  */
@@ -25,13 +28,23 @@ export default {
       },
       defaultValue: 'Yale',
     },
+    spacing: {
+      type: 'select',
+      options: ['Cozy', 'Compact'],
+      mapping: {
+        Cozy: '2rem',
+        Compact: '1rem',
+      },
+      defaultValue: 'Cozy',
+    },
   },
 };
 
-export const radiusAndColor = ({ borderRadius, color }) => {
+export const radiusAndColor = ({ borderRadius, color, spacing }) => {
   const root = document.documentElement;
   root.style.setProperty('--border-radius', borderRadius);
   root.style.setProperty('--c-primary', color);
+  root.style.setProperty('--grid-gutter', spacing);
 
   return `
     <p>You can define your site-wide settings on this page, and they will take effect throughout Storybook. Choose your site settings in the "Controls" tab.</p>
@@ -39,5 +52,7 @@ export const radiusAndColor = ({ borderRadius, color }) => {
     <div style="background: var(--c-primary); height: 100px; width: 100px;"></div>
     <h2>Border Radius</h2>
     <div style="padding: 2rem; display: inline-block; border: 1px solid; border-radius: var(--border-radius);">Check out my border radius!</div>
+    <h2>Spacing</h2>
+    ${gridTwig(gridData)}
   `;
 };
