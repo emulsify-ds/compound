a91ba3e865fe903dbd50a11d8adbc5bc7a7ffb43
Patrick Coffey <patrickcoffey48@gmail.com>
2021-11-19 17:12:36 -0600

    build: set up semantic-release, as well as a release workflow

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: ['main'],
+  repositoryUrl: 'git@github.com:emulsify-ds/compound.git',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
