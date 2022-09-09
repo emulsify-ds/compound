34070d7511df10d1c5a19b17bc267dfa5dc9b69b
Gregg Hendrix <gregg@fourkitchens.com>
2022-09-09 16:49:22 -0700

    feat: add controls for positioning in storybook

--- components/02-molecules/text-with-media/text-with-media.stories.js
+++ components/02-molecules/text-with-media/text-with-media.stories.js
@@ -1,7 +1,40 @@
-import textWithMedia from './text-with-media.twig';
+import textWithMediaTwig from './text-with-media.twig';
 
 import textWithMediaData from './text-with-media.yml';
 
-export default { title: 'Molecules/Text With Media' };
+/**
+ * Storybook Definition.
+ */
+export default {
+  title: 'Molecules/Text With Media',
+  argTypes: {
+    position: {
+      name: 'Positioning',
+      type: 'select',
+      options: ['left', 'right'],
+      defaultValue: 'left',
+    },
+    textWithMediaContent: {
+      name: 'Text With Media Content (optional)',
+      type: 'string',
+      defaultValue: textWithMediaData.text_with_media__content,
+    },
+    textWithMediaGroup: {
+      name: 'Text With Media Group',
+      type: 'string',
+      defaultValue: textWithMediaData.text_with_media__group,
+    },
+  },
+};
 
-export const textWithMediaExample = () => textWithMedia(textWithMediaData);
+export const textWithMedia = ({
+  position,
+  textWithMediaContent,
+  textWithMediaGroup,
+}) =>
+  textWithMediaTwig({
+    ...textWithMediaData,
+    text_with_media__position: position,
+    text_with_media__content: textWithMediaContent,
+    text_with_media__group: textWithMediaGroup,
+  });
